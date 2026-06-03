2527df9f786f4ce3ca3337dce14e9a3e1f91b33f
Jumbowl <collinxchu@gmail.com>
2026-06-03 02:54:08 -0300

    Add accepted items as of 2026-06-02

---
+++ All of this, and yet STILL no Update/RUN THIS BEFORE UPDATING.bat
@@ -0,0 +1,7 @@
+RD "base\characters\Gumshoe" /s /q
+RD "base\characters\Gumshoe_AAI" /s /q
+RD "base\characters\Harvey" /s /q
+
+del "base\sounds\general\sfx-bowl-foiled.opus"
+echo "completed"
+exit
