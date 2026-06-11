479d34a9a9edac99054d12dc652a479513788256
Jumbowl <collinxchu@gmail.com>
2026-06-11 20:10:35 -0300

    Botch Correction Pass 1
    Wahey

--- All of this, and yet STILL no Update/RUN THIS BEFORE UPDATING.bat
+++ All of this, and yet STILL no Update/RUN THIS BEFORE UPDATING.bat
@@ -1,6 +1,9 @@
 RD "base\characters\Gumshoe" /s /q
 RD "base\characters\Gumshoe_AAI" /s /q
 RD "base\characters\Harvey" /s /q
+RD "base\characters\Aigis" /s /q
+RD "base\characters\Aigis_Visor" /s /q
+RD "base\characters\Apollo_Pro" /s /q
 
 del "base\sounds\general\sfx-bowl-foiled.opus"
 echo "completed"
